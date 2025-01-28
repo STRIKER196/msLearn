a6bd55caad84bea426177f6b1a0936fc406b78fe
Arek Stanik <arekstanik196@gmail.com>
2025-01-28 23:28:50 +0100

    Zadania #1

--- msLearn/Program.cs
+++ msLearn/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace msLearn // Note: actual namespace depends on the project name.
 {
@@ -6,7 +8,52 @@ namespace msLearn // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Create and run simple C# console applications (Get started with C#, Part 2)
+            Projekt wyzwania — opracowywanie struktur foreach i if-elseif-elseif do przetwarzania danych tablicy w języku C#
+            #1
+            Projekt kodu starter dla tego modułu to aplikacja konsolowa języka C#, która implementuje następujące funkcje kodu:
+
+            Używa tablic do przechowywania nazw uczniów i wyników przydziałów.
+
+            foreach Używa instrukcji do iterowania nazw uczniów jako zewnętrznej pętli programu.
+
+            if Używa instrukcji w pętli zewnętrznej, aby zidentyfikować nazwę bieżącego ucznia i uzyskać dostęp do wyników przydziału tego ucznia.
+
+            foreach Używa instrukcji w pętli zewnętrznej, aby iterować przez tablicę wyników przydziału i sumować wartości.
+
+            Używa algorytmu w pętli zewnętrznej, aby obliczyć średni wynik egzaminu dla każdego ucznia.
+
+            if-elseif-else Użyj konstrukcji w pętli zewnętrznej, aby ocenić średni wynik egzaminu i automatycznie przypisać klasę listową.
+
+            Integruje dodatkowe oceny kredytowe podczas obliczania końcowego wyniku i oceny litery studenta w następujący sposób:
+
+            Wykrywa dodatkowe przydziały kredytowe na podstawie liczby elementów w tablicy wyników ucznia.
+            Stosuje współczynnik wagi 10% do dodatkowych przypisań kredytowych przed dodaniem dodatkowych wyników kredytowych do sumy wyników egzaminów.
+            Twoim celem w tym zadaniu jest zaimplementowanie aktualizacji kodowania wymaganych do utworzenia żądanego raportu oceny nauczyciela.
+
+            Bieżący raport oceny zawiera nazwę ucznia, po którym następuje obliczona ogólna ocena i ocena listu. Oto istniejący format raportu:
+
+            Output
+            Student         Grade   Letter Grade
+
+            Sophia          95.6    A
+            Andrew          91.6    A-
+            Emma            89.2    B+
+            Logan           93      A
+
+            Oprócz końcowego wyniku liczbowego i klasy listowej ucznia, nauczyciel chce, aby zaktualizowany raport zawierał wynik egzaminu i wpływ dodatkowej pracy kredytowej na końcową ocenę ucznia. Format zaktualizowanego raportu oceny powinien wyglądać następująco:
+
+            Output
+            Student         Exam Score      Overall Grade   Extra Credit
+
+            Sophia          92.2            95.88   A       92 (3.68 pts)
+            Andrew          89.6            91.38   A-      89 (1.78 pts)
+            Emma            85.6            90.94   A-      89 (5.34 pts)
+            Logan           91.2            93.12   A       96 (1.92 pts)
+            */
+
         }
+
+
     }
 }
