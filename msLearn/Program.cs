4829fd2ff414c2d4c911f28edf6731fad467308a
Arek Stanik <arekstanik196@gmail.com>
2025-01-29 14:11:54 +0100

    update

--- msLearn/Program.cs
+++ msLearn/Program.cs
@@ -10,37 +10,94 @@ namespace msLearn // Note: actual namespace depends on the project name.
         {
             /*Ćwiczenie — tworzenie tablic i pętli foreach
             */
-            
 
-            int[] wynikSophi = new int[] { 90, 86, 87, 98, 100 };
-            int[] wynikAndrew = new int[] { 92, 85, 87, 98, 68 };
-            int[] wynikEmmy = new int[] { 90, 85, 87, 88, 96 };
-            int[] wynikLogana = new int[] { 90, 95, 87, 88, 96 };
+            // initialize variables - graded assignments 
+            // initialize variables - graded assignments 
+            int currentAssignments = 5;
 
-            string[] imionaStudentow = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
-            int iloscOcenWZadaniach = wynikSophi.Length;
+            int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
+            int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
+            int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
+            int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
-            foreach (string imie in imionaStudentow)
+            // Student names
+            string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+
+            int[] studentScores = new int[10];
+            string currentStudentLetterGrade = "";
+
+            // Write the Report Header to the console
+            Console.WriteLine("Student\t\tGrade\t\tGradffe\n");
+
+            foreach (string name in studentNames)
             {
-                if (imie == "Sophia")
-                { 
-                    int sumaPunktowSophi = 0;
-                    decimal punktySophi;
+                string currentStudent = name;
 
-                    foreach (int punkty in wynikSophi)
-                    {
-                        sumaPunktowSophi += punkty;
-                    }
+                if (currentStudent == "Sophia")
+                    studentScores = sophiaScores;
 
+                else if (currentStudent == "Andrew")
+                    studentScores = andrewScores;
 
-                    punktySophi = (decimal)sumaPunktowSophi / iloscOcenWZadaniach;
+                else if (currentStudent == "Emma")
+                    studentScores = emmaScores;
 
-                    Console.WriteLine("Student\t\tGrade\n");
-                    Console.WriteLine("Sophia:\t\t" + punktySophi + "\tA-");
+                else if (currentStudent == "Logan")
+                    studentScores = loganScores;
+
+                // initialize/reset the sum of scored assignments
+                int sumAssignmentScores = 0;
+
+                // initialize/reset the calculated average of exam + extra credit scores
+                decimal currentStudentGrade = 0;
+
+                foreach (int score in studentScores)
+                {
+                    // add the exam score to the sum
+                    sumAssignmentScores += score;
                 }
-            }
 
-            Console.WriteLine("Press the Enter key to continue");
+                currentStudentGrade = (decimal)(sumAssignmentScores) / currentAssignments;
+
+                if (currentStudentGrade >= 97)
+                    currentStudentLetterGrade = "A+";
+
+                else if (currentStudentGrade >= 93)
+                    currentStudentLetterGrade = "A";
+
+                else if (currentStudentGrade >= 90)
+                    currentStudentLetterGrade = "A-";
+
+                else if (currentStudentGrade >= 87)
+                    currentStudentLetterGrade = "B+";
+
+                else if (currentStudentGrade >= 83)
+                    currentStudentLetterGrade = "B";
+
+                else if (currentStudentGrade >= 80)
+                    currentStudentLetterGrade = "B-";
+
+                else if (currentStudentGrade >= 77)
+                    currentStudentLetterGrade = "C+";
+
+                else if (currentStudentGrade >= 73)
+                    currentStudentLetterGrade = "C";
+
+                else if (currentStudentGrade >= 70)
+                    currentStudentLetterGrade = "C-";
+
+                else if (currentStudentGrade >= 67)
+                    currentStudentLetterGrade = "D+";
+
+                else if (currentStudentGrade >= 63)
+                    currentStudentLetterGrade = "D";
+
+                else if (currentStudentGrade >= 60)
+                    currentStudentLetterGrade = "D-";
+
+                Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade} points\t{currentStudentLetterGrade}");
+            }
+            Console.WriteLine("\nPress the Enter key to continue");
             Console.ReadLine();
         }
 
