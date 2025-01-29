f6dc4d994e0976f1e0f72e19f435a374ec0dc570
Arek Stanik <arekstanik196@gmail.com>
2025-01-29 17:59:00 +0100

    Obliczanie sredniej dla kazdego ucznia i justowanie tekstu

--- msLearn/Program.cs
+++ msLearn/Program.cs
@@ -1,47 +1,55 @@
 ﻿using System;
 
-namespace msLearn // Note: actual namespace depends on the project name.
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        decimal currentAssignments = 5;
+
+        string[] studentsName = new string[] { "Sofii", "Andrzej", "Emilia", "Jonasz" };
+        int[] sofiiScores = new int[] { 90, 86, 87, 98, 100 };
+        int[] andrzejScores = new int[] { 92, 89, 81, 96, 90 };
+        int[] emiliaScores = new int[] { 90, 85, 87, 98, 68 };
+        int[] jonaszScores = new int[] { 90, 95, 87, 88, 96 };
+
+        int[] currentScoreHolder = new int[5];
+
+        Console.WriteLine($"{"Student".PadRight(10)} {"Grade",8}");
+
+
+
+        foreach (string name in studentsName)
         {
+            int studentScoresSum = 0;
+            decimal studentResoult = 0;
 
-            // initialize variables - graded assignments 
-            int currentAssignments = 5;
-
-            string[] studentsName = new string[] { "Sofii", "Andrzej", "Emilia", "Jonasz"};
-            int[] sofiiScores = new int[] {90, 86, 87, 98, 100};
-            int[] andrzejScores = new int[] { 92, 89, 81, 96, 90 };
-            int[] emiliaScores = new int[] {90, 85, 87, 98, 68};
-            int[] jonaszScores = new int[] {90, 95, 87, 88, 96};
-
-            Console.WriteLine("Student\t\tGrade\n");
-            decimal sofiiScore;
-            decimal andrewScore;
-            decimal emmaScore;
-            decimal loganScore;
-
-            foreach (string name in studentsName)
-            { 
-
-                int sofiiSum = 0;
-                int andrzejSum = 0;
-                foreach (int points in sofiiScores)
-                {
-                    if (name == "Sofii")
-                    { sofiiSum += points; }
-                    else if (name == "Andrzej")
-                    { andrzejSum += points; }
-                }
-
-                sofiiScore = (decimal)sofiiSum / currentAssignments;
-
-                Console.WriteLine($"{name}:\t\t" + sofiiScore + "\tA-");
+            if (name == "Sofii")
+            {
+                currentScoreHolder = sofiiScores;
+            }
+            else if (name == "Andrzej")
+            {
+                currentScoreHolder = andrzejScores;
+            }
+            else if (name == "Emilia")
+            {
+                currentScoreHolder = emiliaScores;
+            }
+            else if (name == "Jonasz")
+            {
+                currentScoreHolder = jonaszScores;
             }
 
-            Console.WriteLine("Press the Enter key to continue");
-            Console.ReadLine();
+            foreach (int points in currentScoreHolder)
+            {
+                studentScoresSum += points;
+                studentResoult = (decimal)studentScoresSum / currentAssignments;
+            }
+
+            Console.WriteLine($"{name.PadRight(10)} {studentResoult,8:F2}  A-");
         }
+
+        Console.WriteLine("\nPress the Enter key to continue");
+        Console.ReadLine();
     }
 }
