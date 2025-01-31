cc2a57fdf4e77c58be142498d4c5ca70243d1540
Arek Stanik <arekstanik196@gmail.com>
2025-01-31 20:37:15 +0100

    Update Cwiczenie fizz 3 bizz 5 fizzbizz 15 dla petli FOR

--- msLearn/Program.cs
+++ msLearn/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Numerics;
+using System.Reflection.Metadata;
 
 namespace msLearn // Note: actual namespace depends on the project name.
 {
@@ -6,7 +8,44 @@ namespace msLearn // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*//PRZYDATNE, WYPISYWANIE TABLICY OD KONCA DO PRZODU
+             * 
+            string[] names = { "Alex", "Eddie", "David", "Michael" };
+            for (int i = names.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(names[i]);
+            }
+            
+            //Update
+            string[] names = { "Alex", "Eddie", "David", "Michael" };
+            for (int i = 0; i < names.Length; i++)
+                if (names[i] == "David") names[i] = "Sammy";
+
+            foreach (var name in names) Console.WriteLine(name);
+            */
+            string fizzName = "Fizz";
+            string buzzName = "Buzz";
+
+            for (int number = 1; number <= 100; number++)
+            {
+
+                if ((number % 3 == 0) && (number % 5 == 0))
+                {
+                    Console.WriteLine($"{number} - {fizzName}{buzzName}");
+                }
+                else if  (number % 3 == 0)
+                {
+                     Console.WriteLine($"{number} - {fizzName}");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine($"{number} - {buzzName}");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+            }
         }
     }
 }
