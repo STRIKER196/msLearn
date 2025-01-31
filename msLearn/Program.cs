4e46765621d89a4b3f63247ed2f92728a39ffc82
Arek Stanik <arekstanik196@gmail.com>
2025-01-31 20:44:52 +0100

    Cwiczenie na bazie lokalizacji zmiennych wewnatrz skladni i poza nią. Czyli takie sprawdzanie kodu i weryfikacja wiedzy o tym jak dzialaja zmiennie i kiedy sa i gdzie sa dostepne

--- msLearn/Program.cs
+++ msLearn/Program.cs
@@ -6,7 +6,44 @@ namespace msLearn // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* 
+            Variable scope challenge
+            In this challenge, you'll use what you've learned about code blocks and variable scope
+            to fix the poorly written code sample provided.
+            There are many improvements that you can make. Good luck!
+
+            int[] numbers = { 4, 8, 15, 16, 23, 42 };
+            foreach (int number in numbers)
+            {
+                int total;
+                total += number;
+                if (number == 42)
+                {
+                   bool found = true;
+                }
+            }
+            if (found) 
+            {
+                Console.WriteLine("Set contains 42");
+            }
+            Console.WriteLine($"Total: {total}");
+             */
+
+            int[] numbers = { 4, 8, 15, 16, 23, 42 };
+            int total = 0;
+            bool found = false;
+
+            foreach (int number in numbers)
+            {
+                total += number;
+                if (number == 42)
+                    found = true;
+            }
+
+            if (found)
+                Console.WriteLine("Set contains 42");
+
+            Console.WriteLine($"Total: {total}");
         }
     }
 }
