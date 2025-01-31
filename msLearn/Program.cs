08a97db4cd18f7420c74448ff6b464801b662700
Arek Stanik <arekstanik196@gmail.com>
2025-01-31 23:18:13 +0100

    Wprowadzenie opisu ćwiczenia - gra

--- msLearn/Program.cs
+++ msLearn/Program.cs
@@ -7,8 +7,26 @@ namespace msLearn // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             /*
-             * 
-             */
+            Random random = new Random();
+            int current = random.Next(1, 11);
+            do
+            {
+                current = random.Next(1, 11);
+                Console.WriteLine(current);
+            } while (current != 7);
+            while (current >= 3)
+            {
+                Console.WriteLine(current);
+                current = random.Next(1, 11);
+            }
+            Console.WriteLine($"Last number: {current}");
+
+
+            //ĆWICZENIE LEKCJI: Stworzenie prostej gry na bazie do-while, które polega na zbijaniu punktów zdrowia.
+            Bohater jak i przeciwnik mają po 10 punktów zdrowia. każdy z nich moze zatakować raz na turę.
+            Wartość ataków obejmuje rzut kostką (1-6). Całość kończy się w momencie pokonania jednego z postaci.
+            Gra może być przerwana, zrestartowana lub wyłączona. Na koniec gry jesteś pytany o restart lub koniec.
+            */
         }
     }
 }
