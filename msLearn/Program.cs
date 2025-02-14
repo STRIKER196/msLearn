bc67e00e8ef85097c4948aa3c1f48657389e771d
Arek Stanik <arekstanik196@gmail.com>
2025-02-14 19:00:25 +0100

    3 zadania 2/3 zrobione przez mnie.

--- msLearn/Program.cs
+++ msLearn/Program.cs
@@ -8,34 +8,35 @@ namespace msLearn // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            string? userValue;
+            string? readUserValue;
             bool dataInput = false;
             int tryTaskOneCount = 1;
+            int userReadText;
 
             Console.WriteLine("Podaj wartość, która mieście się w zakresie liczbowym 5-10");
             do
             {
                 Console.WriteLine($"Próba nr: {tryTaskOneCount}");
-                userValue = Console.ReadLine();
-                if (userValue != null)
+                readUserValue = Console.ReadLine();
+                if (readUserValue != null) // założenie ćwiczenia wiec musi zostać
                 {
-                    if (int.TryParse(userValue, out var userReadText))
+                    if (int.TryParse(readUserValue, out userReadText))
                     {
-                        if (userReadText >= 5 && userReadText <= 10)
+                        if (userReadText > 5 && userReadText < 10)
                         {
-                            
+
                             Console.Write($"Twoja wartość to: \"");
                             Console.ForegroundColor = ConsoleColor.Green;
-                            Console.Write($"{userValue}");
+                            Console.Write($"{readUserValue}");
                             Console.ResetColor();
                             Console.WriteLine("\" i jest poprawna.");
                             dataInput = true;
                         }
-                        else 
+                        else
                         {
-                            Console.ForegroundColor = ConsoleColor.Red;
                             Console.Write("Najwyraźniej twoja wartość jest nieprawidłowa.");
-                            Console.WriteLine($" \"{userValue}\"");
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($" \"{readUserValue}\"");
                             Console.ResetColor();
                             Console.WriteLine("Przeczytaj instrukcję raz jeszcze i dostosuj się do niej.");
                         }
@@ -50,7 +51,7 @@ namespace msLearn // Note: actual namespace depends on the project name.
 
                 tryTaskOneCount++;
 
-            }while (dataInput == false);
+            } while (!dataInput);
 
             Console.WriteLine("\n\nDruga część zadania");
 
@@ -65,7 +66,7 @@ namespace msLearn // Note: actual namespace depends on the project name.
             Console.WriteLine($"Dostępne role to:\n{adm}\t{man}\t{usr}");
 
 
-            do 
+            do
             {
                 Console.WriteLine($"Próba weryfikacji roli nr: {tryTaskTwoCount}");
                 Console.WriteLine("Wprowadz nazwę roli");
@@ -85,7 +86,7 @@ namespace msLearn // Note: actual namespace depends on the project name.
                         Console.WriteLine(") została zaakceptowana");
                         verify = true;
                     }
-                    else 
+                    else
                     {
                         Console.Write("Rola która wprowadzono '");
                         Console.ForegroundColor = ConsoleColor.Red;
@@ -112,6 +113,44 @@ namespace msLearn // Note: actual namespace depends on the project name.
             while (!verify);
             Console.WriteLine("Logowanie udane :)");
             Console.ReadLine();
+            // gotowe ćwicznie nie robiłemgo
+            Console.WriteLine("\n\nTrzecia część zadania");
+
+            string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+            int stringsCount = myStrings.Length;
+
+            string myString = "";
+            int periodLocation = 0;
+
+            for (int i = 0; i < stringsCount; i++)
+            {
+                myString = myStrings[i];
+                periodLocation = myString.IndexOf(".");
+
+                string mySentence;
+
+                // extract sentences from each string and display them one at a time
+                while (periodLocation != -1)
+                {
+
+                    // first sentence is the string value to the left of the period location
+                    mySentence = myString.Remove(periodLocation);
+
+                    // the remainder of myString is the string value to the right of the location
+                    myString = myString.Substring(periodLocation + 1);
+
+                    // remove any leading white-space from myString
+                    myString = myString.TrimStart();
+
+                    // update the comma location and increment the counter
+                    periodLocation = myString.IndexOf(".");
+
+                    Console.WriteLine(mySentence);
+                }
+
+                mySentence = myString.Trim();
+                Console.WriteLine(mySentence);
+            }
         }
     }
 }
