f82c70491cae7a45f3299fceacc30b76b2b548b4
Arek Stanik <arekstanik196@gmail.com>
2025-02-15 12:47:15 +0100

    v2

--- msLearn/Program.cs
+++ msLearn/Program.cs
@@ -2,13 +2,13 @@
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 
-namespace msLearn // Note: actual namespace depends on the project name.
+namespace msLearn 
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // the ourAnimals array will store the following: 
+            //zmienne wspisywane do tablicy
             string animalSpecies = "";
             string animalID = "";
             string animalAge = "";
@@ -16,22 +16,13 @@ namespace msLearn // Note: actual namespace depends on the project name.
             string animalPersonalityDescription = "";
             string animalNickname = "";
 
-            // variables that support data entry
             int maxPets = 8;
             string? readResult;
             string menuSelection = "";
 
-            // array used to store runtime data, there is no persisted data
             string[,] ourAnimals = new string[maxPets, 6];
 
-            // TODO: Convert the if-elseif-else construct to a switch statement
-
-            switch ()
-            {
-                case:
-                    break;
-            }
-            // create some initial ourAnimals array entries
+            //wartości zmiennych wpisane do tablicy
             for (int i = 0; i < maxPets; i++)
             {
                 if (i == 0)
@@ -88,23 +79,29 @@ namespace msLearn // Note: actual namespace depends on the project name.
                 ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
             }
 
-            // display the top-level menu options
 
             Console.Clear();
 
-            Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
-            Console.WriteLine(" 1. List all of our current pet information");
-            Console.WriteLine(" 2. Add a new animal friend to the ourAnimals array");
-            Console.WriteLine(" 3. Ensure animal ages and physical descriptions are complete");
-            Console.WriteLine(" 4. Ensure animal nicknames and personality descriptions are complete");
-            Console.WriteLine(" 5. Edit an animal’s age");
-            Console.WriteLine(" 6. Edit an animal’s personality description");
-            Console.WriteLine(" 7. Display all cats with a specified characteristic");
-            Console.WriteLine(" 8. Display all dogs with a specified characteristic");
-            Console.WriteLine();
-            Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
+            Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:\n");
 
-            readResult = Console.ReadLine();
+            Console.WriteLine("+----------+-------------------------------------------------------------------+");
+            Console.WriteLine("| Pozycja  | Opis działania                                                    |");
+            Console.WriteLine("+----------+-------------------------------------------------------------------+");
+            Console.WriteLine("| 1.       | List all of our current pet information                           |");
+            Console.WriteLine("| 2.       | Add a new animal friend to the ourAnimals array                   |");
+            Console.WriteLine("| 3.       | Ensure animal ages and physical descriptions are complete         |");
+            Console.WriteLine("| 4.       | Ensure animal nicknames and personality descriptions are complete |");
+            Console.WriteLine("| 5.       | Edit an animal’s age                                              |");
+            Console.WriteLine("| 6.       | Edit an animal’s personality description                          |");
+            Console.WriteLine("| 7.       | Display all cats with a specified characteristic                  |");
+            Console.WriteLine("| 8.       | Display all dogs with a specified characteristic                  |");
+            Console.WriteLine("+----------+-------------------------------------------------------------------+");
+            Console.WriteLine("| Exit     | Wpisz słowo \"Exit\", wby wyjść z programu                          |");
+            Console.WriteLine("+----------+-------------------------------------------------------------------+");
+
+            Console.WriteLine("\nEnter your selection number (or type Exit to exit the program)");
+
+            readResult = Console.ReadLine().ToLower() ?? "";
             if (readResult != null)
             {
                 menuSelection = readResult.ToLower();
@@ -112,6 +109,34 @@ namespace msLearn // Note: actual namespace depends on the project name.
 
             Console.WriteLine($"You selected menu option {menuSelection}.");
             Console.WriteLine("Press the Enter key to continue");
+
+
+            //List all 1
+
+
+            for (int i = 0; i < 8; i++)
+            {
+
+                Console.WriteLine("+--------------------------+------------------------------------------+");
+                Console.WriteLine($"|\tProperty\t\t\t\t\t\tindex: i ={i}\t\t|\tValue\t\t\t\t|");
+                Console.WriteLine("+--------------------------+------------------------------------------+");
+                
+                for (j = 0; j < 5; j++)
+                {
+                    Console.WriteLine("+--------------------------+------------------------------------------+");
+                    Console.WriteLine($"|{ourAnimals[i, j]}");
+                    Console.WriteLine("+--------------------------+------------------------------------------+");
+                }
+                Console.WriteLine("");
+
+            }
+
+            //ourAnimals[i, 0] = "ID #: " + animalID;
+            //ourAnimals[i, 1] = "Species: " + animalSpecies;
+            //ourAnimals[i, 2] = "Age: " + animalAge;
+            //ourAnimals[i, 3] = "Nickname: " + animalNickname;
+            //ourAnimals[i, 4] = "Physical description: " + animalPhysicalDescription;
+            //ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
 
             // pause code execution
             readResult = Console.ReadLine();
